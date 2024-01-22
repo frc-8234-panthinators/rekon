0ee10f73ee0566e5adce5b209a49757dd3149f12
tbuk <jonasargrill@gmail.com>
2024-01-21 23:47:12 -0600

    colorcandidate4 - charcoal moonstone

--- colors.js
+++ colors.js
@@ -1,18 +1,18 @@
 export default Colors = {
-    background: '#0F0A0A',
-    tab: '#333333',
-    component: '#333333',
+    background: '#040F16',
+    tab: '#2B343B',
+    component: '#2B343B',
     mainText: '#EFF1ED',
     subText: '#F7EBEC',
     header: '#BDC7DC',
     border: '#858585',
     tabIcons: '#e3e2e6',
-    tabSelected: '#211F1F',
-    blue: '#1768AC',
-    red: '#A30018',  
-    innerComponent: '#858585',
+    tabSelected: '#1B252B',
+    blue: '#066AB1',
+    red: '#A61128',  
+    innerComponent: '#64717D',
     PieGreen: '#58ba6d',
     PieRed: '#9a5c5c',
-    accent: '#C88304'
+    accent: '#0098A3'
     
 }
