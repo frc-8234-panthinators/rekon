f5844c15e799777a231ca9fb0b3e61ae2613422d
tbuk <jonasargrill@gmail.com>
2024-01-21 23:40:24 -0600

    colorcandidate3 - rusty orange

--- colors.js
+++ colors.js
@@ -1,18 +1,18 @@
 export default Colors = {
-    background: '#0C0910',
-    tab: '#312541',
-    component: '#312541',
-    mainText: '#bdc7dc',
-    subText: '#e3e2e6',
+    background: '#0F0A0A',
+    tab: '#333333',
+    component: '#333333',
+    mainText: '#EFF1ED',
+    subText: '#F7EBEC',
     header: '#BDC7DC',
-    border: '#8E9099',
+    border: '#858585',
     tabIcons: '#e3e2e6',
-    tabSelected: '#1F1729',
+    tabSelected: '#211F1F',
     blue: '#1768AC',
     red: '#A30018',  
-    innerComponent: '#AA8DCE',
+    innerComponent: '#858585',
     PieGreen: '#58ba6d',
     PieRed: '#9a5c5c',
-    accent: '#8D53D4'
+    accent: '#C88304'
     
 }
